d226b02bf0de3f10be6d46403e7470037a94a345
natasha17-spec <472682289popsik>
2020-05-23 21:56:43 +0300

    1

--- JS.js
+++ JS.js
@@ -1,4 +1,51 @@
-function foo(a){
-    console.log(a)
+//21
+// function now() {
+//     return 21
+//
+// }
+// function later() {
+//     answer = answer*2
+//     console.log("Meaning of life:",answer)
+//
+// }
+// let answer = now();
+// setTimeout(later,3000);
+
+//22
+// var a = {
+//     index:1
+// }
+// //!позднее
+// console.log(a)//*1
+// a.index++
+// console.log(a)//*2
+
+//25
+// let eventloop = [];
+// let event;
+// while(true){
+//     if (eventloop.length > 0) {
+//         event = eventloop.shift()
+//         try {
+//             event()
+//         }
+//         catch (err) {
+//             reportError(err)
+//         }
+//     }
+// }
+//28
+let a = 20;
+
+function foo() {
+    a = a + 1
 }
-foo(2)
+
+function bar() {
+    a = a + 2
+}
+
+ajax('http://some.url1.1', foo)
+ajax('http://some.url1.2', foo)
+
+
